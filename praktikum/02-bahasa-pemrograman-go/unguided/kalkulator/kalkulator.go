6673e8ddd7b943eabd386cc6bf2f7d07f79348ef
alfsteve <alfstevechta@gmail.com>
2026-09-23 20:27:42 +0700

    Initial commit

---
+++ praktikum/02-bahasa-pemrograman-go/unguided/kalkulator/kalkulator.go
@@ -0,0 +1,26 @@
+package main
+
+	import "fmt"
+	
+	func main() {
+		var a, b int
+
+		//Membaca input
+		fmt.Print("Masukkan nilai a:")
+		fmt.Scan(&a)
+		fmt.Print("Masukkan nilai b:")
+		fmt.Scan(&b)
+
+		//Menampilkan output
+		fmt.Print("Hasil Penjumlahan: ")
+		fmt.Println(a + b)
+		fmt.Print("Hasil Pengurangan: ")
+		fmt.Println(a - b)
+		fmt.Print("Hasil Perkalian: ")
+		fmt.Println(a * b)
+		fmt.Print("Hasil Pembagian: ")
+		fmt.Println(a / b)
+		fmt.Print("Hasil Persen: ")
+		fmt.Println(a % b)
+
+}
