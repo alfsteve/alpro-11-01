3fc6e3bbfd4762480468c3b411edd2802a193d31
alfsteve <alfstevechta@gmail.com>
2026-09-26 13:12:50 +0700

    Initial commit

--- praktikum/02-bahasa-pemrograman-go/unguided/cacahuang/cacahuang.go
+++ praktikum/02-bahasa-pemrograman-go/unguided/cacahuang/cacahuang.go
@@ -3,18 +3,24 @@ package main
 	import "fmt"
 
 	func main() {
-		var a, b, c int
+		var a int
+		var sepuluh, lima, satu int
+		var b int
 
 		//Membaca input
-		fmt.Print("Masukkan nilai a: ")
+		fmt.Print("Masukkan nilai uang: ")
 		fmt.Scan(&a)
-		fmt.Print("Masukkan nilai b: ")
-		fmt.Scan(&b)
-		fmt.Print("Masukkan nilai c: ")
-		fmt.Scan(&c)
 
 		//Menampilkan output
-		fmt.Print("Hasilnya : ", a, " * 10000 + ", b, " * 5000 + ", c, " * 1000 = ")
-		fmt.Println(a * 10000 + b * 5000 + c * 1000)
+		sepuluh = a / 10000
+		b = a % 10000
+		lima = b / 5000
+		b = b % 5000
+		satu = b / 1000
+
+		fmt.Println("Pecahan uang:")
+		fmt.Println("10.000:", sepuluh)
+		fmt.Println("5.000:", lima)
+		fmt.Println("1.000:", satu)
 		
 	}
